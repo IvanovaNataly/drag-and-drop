f27893ed69a6d2cefd56cec68a6c0c3e511c0ccc
NatalyIvanova <nataly.ivanova@clicktale.com>
2019-01-16 11:34:19 +0200

    url

--- search.js
+++ search.js
@@ -1,24 +1,5 @@
-// const fPages = '<li class="ui-widget-content title" data-title="Location">'+
-//     '<span class="filter-name">Pages</span>'+
-//     '<select class="location-select">'+
-//     '<option value="URL">Page URL</option>'+
-//     '<option value="Attribute">Page Attribute</option>'+
-//     '<option value="Pageset">Pageset</option>'+
-//     '<option value="Query string">Query string</option>'+
-//     '</select>'+
-//     '<select class="condition-type">'+
-//     '<option value="equals">equals</option>'+
-//     '<option value="contains">contains</option>'+
-//     '<option value="starts with">starts with</option>'+
-//     '<option value="ends with">ends with</option>'+
-//     '<option value="is NOT">is NOT</option>'+
-//     '<option value="does not contain">does not contain</option>'+
-//     '<option value="does not starts with">does not starts with</option>'+
-//     '<option value="does not ends with">does not ends with</option>'+
-//     '</select>'+
-//     '<input type="text" placeholder="Type" class="filter-input">'+
-//     '<span class="iconFont trash"></span></li>';
 import { Pages } from './pages.js';
+import { Url } from './url.js';
 
 $( function() {
     //Constant set of filters
@@ -295,7 +276,9 @@ $( function() {
     function addElements() {
         // $gallery.empty().append(filters);
         let pages = new Pages();
-        $gallery.empty().append(pages.renderPages());
+        let url = new Url();
+        let filters = pages.renderPages() + url.renderUrl('Visited Pages');
+        $gallery.empty().append(filters);
     }
 
     function deleteItem(event) {
