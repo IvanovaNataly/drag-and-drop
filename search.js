edecc1f2a170cdac861dd119f171befba62a9dfc
NatalyIvanova <nataly.ivanova@clicktale.com>
2019-01-16 11:18:04 +0200

    filter class imported

--- search.js
+++ search.js
@@ -1,13 +1,45 @@
+// const fPages = '<li class="ui-widget-content title" data-title="Location">'+
+//     '<span class="filter-name">Pages</span>'+
+//     '<select class="location-select">'+
+//     '<option value="URL">Page URL</option>'+
+//     '<option value="Attribute">Page Attribute</option>'+
+//     '<option value="Pageset">Pageset</option>'+
+//     '<option value="Query string">Query string</option>'+
+//     '</select>'+
+//     '<select class="condition-type">'+
+//     '<option value="equals">equals</option>'+
+//     '<option value="contains">contains</option>'+
+//     '<option value="starts with">starts with</option>'+
+//     '<option value="ends with">ends with</option>'+
+//     '<option value="is NOT">is NOT</option>'+
+//     '<option value="does not contain">does not contain</option>'+
+//     '<option value="does not starts with">does not starts with</option>'+
+//     '<option value="does not ends with">does not ends with</option>'+
+//     '</select>'+
+//     '<input type="text" placeholder="Type" class="filter-input">'+
+//     '<span class="iconFont trash"></span></li>';
+import { Pages } from './pages.js';
+
 $( function() {
     //Constant set of filters
-    var filters = '<li class="ui-widget-content title" data-title="Location">Location'+
-            '<select class="location-select"><option value="url">Page URL</option>'+
-                '<option value="url">Page Attribute</option>'+
-                '<option value="url">Pageset</option></select>'+
+    var filters = '<li class="ui-widget-content title" data-title="Location">'+
+        '<span class="filter-name">Pages</span>'+
+            '<select class="location-select">'+
+                '<option value="URL">Page URL</option>'+
+                '<option value="Attribute">Page Attribute</option>'+
+                '<option value="Pageset">Pageset</option>'+
+                '<option value="Query string">Query string</option>'+
+            '</select>'+
             '<select class="condition-type">'+
-                '<option value="none"></option>'+
-                '<option value="empty">-</option>'+
-                '<option value="not">NOT</option></select>'+
+                '<option value="equals">equals</option>'+
+                '<option value="contains">contains</option>'+
+                '<option value="starts with">starts with</option>'+
+                '<option value="ends with">ends with</option>'+
+                '<option value="is NOT">is NOT</option>'+
+                '<option value="does not contain">does not contain</option>'+
+                '<option value="does not starts with">does not starts with</option>'+
+                '<option value="does not ends with">does not ends with</option>'+
+            '</select>'+
             '<input type="text" placeholder="Type" class="filter-input">'+
             '<span class="iconFont trash"></span></li>'+
         '<li class="ui-widget-content title" data-title="Behavior">Interaction and sequence'+
@@ -261,7 +293,9 @@ $( function() {
         $trash = $(".level-empty");
 
     function addElements() {
-        $gallery.empty().append(filters);
+        // $gallery.empty().append(filters);
+        let pages = new Pages();
+        $gallery.empty().append(pages.renderPages());
     }
 
     function deleteItem(event) {
---
+++ url.js
@@ -0,0 +1,25 @@
+export class Pages {
+    renderPages() {
+        return ('<li class="ui-widget-content title" data-title="Location">'+
+        '<span class="filter-name">Pages</span>'+
+        '<select class="location-select">'+
+        '<option value="URL">Page URL</option>'+
+        '<option value="Attribute">Page Attribute</option>'+
+        '<option value="Pageset">Pageset</option>'+
+        '<option value="Query string">Query string</option>'+
+        '</select>'+
+        '<select class="condition-type">'+
+        '<option value="equals">equals</option>'+
+        '<option value="contains">contains</option>'+
+        '<option value="starts with">starts with</option>'+
+        '<option value="ends with">ends with</option>'+
+        '<option value="is NOT">is NOT</option>'+
+        '<option value="does not contain">does not contain</option>'+
+        '<option value="does not starts with">does not starts with</option>'+
+        '<option value="does not ends with">does not ends with</option>'+
+        '</select>'+
+        '<input type="text" placeholder="Type" class="filter-input">'+
+        '<span class="iconFont trash"></span></li>'
+    )}
+}
+
