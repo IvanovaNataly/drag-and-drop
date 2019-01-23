77234406d11c490e880d4c227d1ad2c4571e0dd2
Nataly Ivanova <nataliai@netcraft.co.il>
2019-01-23 14:02:56 +0200

    group pills

--- scripts/pill.js
+++ scripts/pill.js
@@ -1,5 +1,6 @@
 export class PillRenderer {
     render(allPills) {
+        const that = this;
         const sum = $.map( allPills, function(pill) {
             if(pill.hasOwnProperty("condition")) return;
             if(pill.name === "Pages")
@@ -8,6 +9,13 @@ export class PillRenderer {
                     pill.name +
                     '</div>'
                 );
+            else if(pill.name === "Group") {
+                return ('<div class="pills-bar-level '+ pill.name.toLowerCase() +'">'+
+                    pill.name + ' level'+
+                        that.render(pill.content) +
+                    '</div>'
+                );
+            }
             else return ('<div class="pill">'+
                 '<div class="icon-action"></div>'+
                 pill.name +
